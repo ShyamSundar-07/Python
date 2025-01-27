d8858e264cb5b0af1ee5183a3052ffeda124a93f
ShyamSundar-07 <shyamgrand005@gmail.com>
2025-01-27 15:20:02 +0530

    Add files via upload

---
+++ ss3.py
@@ -0,0 +1,10 @@
+x=int(input("Enter your marks: "))
+ if x>89:
+   print("A Grade")
+ elif 80<=x<=89:
+   print("B Grade")
+ elif 70<=x<=79:
+   print("C Grade")
+  else:
+   print("Fail")
+   
---
+++ ss5.py
@@ -0,0 +1,10 @@
+n=int(input("Enter a number:"))
+t=n
+rev=0
+while n!=0:
+ r=n%10
+ rev=rev*10+r
+ n=n//10
+ print("Reverse of number",t,"is:",rev)
+
+
